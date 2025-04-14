aba905079c0374763117f940bdead73ec1eb368d
Arpit <arpitjoshi213.com>
2025-04-14 22:39:15 +0530

    making home page better

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,20 @@
-
-
 export default function Footer() {
   return (
     <div className="bg-dark">
-        <div className="text-white flex justify-between">
- <div>
-    
- <p className="p-6 text-lg">© 2025 BootWind. All Rights Reserved.</p>
- </div>
-    <div className="flex">
-    <p className="p-6 text-lg">About</p>
-    <p className="p-6 text-lg">Private policy</p>
-    <p className="p-6 text-lg">Licence</p>
-    <p className="p-6 text-lg">contact</p>
-    </div>
+      <div className="text-white flex flex-col md:flex-row justify-between p-6">
+        {/* Left Section */}
+        <div>
+          <p className="text-lg">© 2025 BootWind. All Rights Reserved.</p>
         </div>
+
+        {/* Right Section */}
+        <div className="flex flex-col md:flex-row mt-4 md:mt-0">
+          <p className="p-2 text-lg">About</p>
+          <p className="p-2 text-lg">Private Policy</p>
+          <p className="p-2 text-lg">Licence</p>
+          <p className="p-2 text-lg">Contact</p>
+        </div>
+      </div>
     </div>
   )
 }
